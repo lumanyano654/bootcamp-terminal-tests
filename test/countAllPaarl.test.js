37856f9d1e99efe6173cec4c5a343afd2bfde9c0
lumanyano654 <gomomolumanyano@gmail.com>
2020-08-19 14:31:48 +0200

    we added test on the mostProfitabke function

--- test/countAllPaarl.test.js
+++ test/countAllPaarl.test.js
@@ -4,20 +4,22 @@ var countAllPaarl = require("../countAllPaarl")
 
 describe("the countAllPaarl function", function() {
     it("should give out all those from Paarl", function() {
-      
-        assert.equal('CJ', countAllPaarl('CJ'))
+      var regNmbrsFrmPaarl = ["CJ 123", "CJ 7523"]
+        assert.deepEqual(regNmbrsFrmPaarl, countAllPaarl('CJ 123, CA 456, CJ 7523'))
   
     })
 
     it("should pass for CJ 5436-74", function() {
-      
-        assert.equal('CJ 5436-74', countAllPaarl('CJ 5436-74'))
+        var regNmbrsFrmPaarl = ["CJ 5436-74"]
+        
+        assert.deepEqual(regNmbrsFrmPaarl, countAllPaarl('CA 453, CJ 5436-74, CY 7864'))
   
     })
 
     it("should pass for CJ 874599", function() {
+        var regNmbrsFrmPaarl = ["CJ 123-43", "CJ 2634-6334", "CJ 235-2367", "CJ 654-3784"]
       
-        assert.equal('CJ 874599', countAllPaarl('CJ 874599'))
+        assert.deepEqual(regNmbrsFrmPaarl, countAllPaarl("CJ 123-43, CJ 2634-6334, CJ 235-2367, CJ 654-3784, CA 23466, CY 7635" ))
   
     })
 
